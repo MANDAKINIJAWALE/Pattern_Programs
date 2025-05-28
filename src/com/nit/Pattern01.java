d8e4de7f411c3341f8d4b4eb3f2bdaca65f5b136
Mandakini <mandakinijawale6@gmail.com>
2025-05-28 16:00:07 +0530

    pattern program

---
+++ src/com/nit/Pattern01.java
@@ -0,0 +1,29 @@
+package com.nit;
+
+import java.util.Scanner;
+
+public class Pattern01 
+{
+	public static void patternMethod(int n)
+	{
+		
+		for(int i=0;i<n;i++)
+		{
+			for(char j=(char) ('E'-i);j<='E';j++)
+			{
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+		
+	}
+	public static void main(String args[])
+	{
+		
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		patternMethod(n);
+		
+	}
+
+}
